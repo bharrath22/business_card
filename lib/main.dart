2220e37f4571462e9d9a0edf8a2921214457e54f
bharrath22 <bharrathnanduri@gmail.com>
2020-11-21 07:35:11 -0500

    Final Commit: Business Card

--- lib/main.dart
+++ lib/main.dart
@@ -10,81 +10,79 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [Colors.red, Colors.blue],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight)),
-          child: SafeArea(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 75,
-                    backgroundImage: AssetImage('images/cr7.jpg'),
-                  ),
-                  Text(
-                    'Christiano Ronaldo',
-                    style: TextStyle(
-                      fontFamily: 'Pacifico',
-                      fontSize: 40.0,
-                      color: Colors.amber[400],
-                    ),
-                  ),
-                  Text(
-                    'Greatest of All Time',
-                    style: TextStyle(
-                        fontFamily: 'Permanent_Marker',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                        color: Colors.cyanAccent),
-                  ),
-                  SizedBox(
-                    height: 20,
-                    width: 200,
-                    child: Divider(
-                      thickness: 3,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  Card(
-                    margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+        backgroundColor: Colors.grey[900],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 75,
+                  backgroundImage: AssetImage('images/jm2.png'),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Jose Mourinho',
+                  style: TextStyle(
+                    fontFamily: 'Castoro',
+                    letterSpacing: 1,
+                    fontSize: 40.0,
                     color: Colors.white,
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.email,
-                        color: Colors.blueAccent,
-                      ),
-                      title: Text(
-                        'cr7@goat.com',
-                        style: TextStyle(
-                            letterSpacing: 2,
-                            fontSize: 25.0,
-                            fontFamily: 'Roboto',
-                            color: Colors.black54,
-                            fontWeight: FontWeight.bold),
-                      ),
+                    shadows:
+                  ),
+                ),
+                Text(
+                  'Greatest of All Time',
+                  style: TextStyle(
+                      fontFamily: 'Permanent_Marker',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: Colors.white70),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 200,
+                  child: Divider(
+                    thickness: 3,
+                    color: Colors.grey,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.blueAccent,
+                    ),
+                    title: Text(
+                      'special1@jm.com',
+                      style: TextStyle(
+                          fontSize: 25.0,
+                          fontFamily: 'Roboto',
+                          color: Colors.grey[800],
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
-                  Card(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                    color: Colors.white,
-                    child: ListTile(
-                      leading: Icon(Icons.phone, color: Colors.redAccent),
-                      title: Text(
-                        '+1-123-456-7890',
-                        style: TextStyle(
-                            fontFamily: 'Roboto',
-                            fontSize: 25,
-                            color: Colors.black54,
-                            fontWeight: FontWeight.bold),
-                      ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.redAccent),
+                    title: Text(
+                      '+1-123-456-7890',
+                      style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 25,
+                          color: Colors.grey[800],
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
